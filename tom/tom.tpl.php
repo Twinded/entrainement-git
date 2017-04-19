14d8cb96a4e172189710d07113111a0e577dfdfa
sauter_t <contact@tcloarec.fr>
2017-04-19 12:08:20 +0200

    Ajout fichiers tom

---
+++ tom/tom.tpl.php
@@ -0,0 +1,7 @@
+<div>
+	<p><span style="font-weight: 600; color: red">Better</span> to use Tortoise</p>
+</div>
+<div>
+	<?php echo ' Le nom de la machine est "' . $_SERVER['REMOTE_HOST'] . '", l\'adresse ip est "' . $_SERVER['REMOTE_ADDR'] . '".'; ?>
+</div>
+<script src="tom.js" type="text/javascript"></script>
