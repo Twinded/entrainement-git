39f44cf0db491d9234f2fc57b8021e575ebbece7
Twinded <web@tcloarec.fr>
2017-04-19 11:21:23 +0200

    Correction main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
+	"use strict";
 	$('#lien0').click(function() {
-		$('#content').load("/utilisation-git/thomas/thomas.php")	
+		$('#content').load('/utilisation-git/thomas/thomas.php');	
 	});
 });
